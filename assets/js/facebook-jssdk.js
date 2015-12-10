8ec62f4b31b3039b386524c2def41b7bff963d05
Binh Duong <quangbinh0108@gmail.com>
2015-12-11 01:57:06 +0700

    Change location

--- assets/js/facebook-jssdk.js
+++ assets/js/facebook-jssdk.js
@@ -3,6 +3,6 @@
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.5&appId=%%APP_ID%%";
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=%%APP_ID%%";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
